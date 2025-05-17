a5d2cf785818b2b58de78d0818e9cad5e07b165d
Prajwal Bharadwaj BM <prajwalbm40@gmail.com>
2025-05-17 19:22:58 +0530

    feat: implement pagination for fetch files endpoint
    - add support for pagination with page and limit query parameters
    - return paginated response including current page, page size, total items, and total pages

--- internal/handlers/fetchFiles.go
+++ internal/handlers/fetchFiles.go
@@ -2,14 +2,41 @@ package handlers
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/prajwalbharadwajbm/gupload/internal/db/repository"
 	"github.com/prajwalbharadwajbm/gupload/internal/interceptor"
 	"github.com/prajwalbharadwajbm/gupload/internal/logger"
 )
 
+var (
+	page  = 1
+	limit = 10
+)
+
 func FetchFiles(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
+
+	pageStr := r.URL.Query().Get("page")
+	limitStr := r.URL.Query().Get("limit")
+
+	if pageStr != "" {
+		pageInt, err := strconv.Atoi(pageStr)
+		if err == nil && pageInt > 0 {
+			page = pageInt
+		} else {
+
+		}
+	}
+
+	if limitStr != "" {
+		limitInt, err := strconv.Atoi(limitStr)
+		if err == nil && limitInt > 0 {
+			limit = limitInt
+		} else {
+
+		}
+	}
 
 	files, err := repository.GetFilesByUserId(ctx)
 	if err != nil {
@@ -18,5 +45,43 @@ func FetchFiles(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	interceptor.SendSuccessResponse(w, files, http.StatusOK)
+	totalFiles := len(files)
+	startIndex := (page - 1) * limit
+	endIndex := startIndex + limit
+
+	if startIndex >= totalFiles {
+		paginatedResponse := map[string]interface{}{
+			"files": []map[string]any{},
+			"pagination": map[string]int{
+				"current_page": page,
+				"page_size":    limit,
+				"total_items":  totalFiles,
+				"total_pages":  (totalFiles + limit - 1) / limit,
+			},
+		}
+		interceptor.SendSuccessResponse(w, paginatedResponse, http.StatusOK)
+		return
+	}
+
+	// 25 files
+	// 3rd page with limit 10
+	// startIndex = (3-1)*10 = 20
+	// endIndex = 20+10 = 30 (30 is out of bound, and will cause panic)
+	if endIndex > totalFiles {
+		endIndex = totalFiles
+	}
+
+	paginatedFiles := files[startIndex:endIndex]
+
+	paginatedResponse := map[string]interface{}{
+		"files": paginatedFiles,
+		"pagination": map[string]int{
+			"current_page": page,
+			"page_size":    limit,
+			"total_items":  totalFiles,
+			"total_pages":  (totalFiles + limit - 1) / limit,
+		},
+	}
+
+	interceptor.SendSuccessResponse(w, paginatedResponse, http.StatusOK)
 }
